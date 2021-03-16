f388e23bf54e60c7e337f586b66a83d66066fc51
tomo-pragmatic <prettysillything@gmail.com>
2021-03-17 08:58:57 +0900

    modified.

--- js/script.js
+++ js/script.js
@@ -2,17 +2,119 @@ const CANVAS_QUIZ = 'canvas-quiz';
 const CANVAS_INFO = 'canvas-info';
 
 var quiz, info;
+var option1, option2, option3, option4, option5, option6;
+var toNext;
 
 function changeLayer(front) {
   if (front == 'quiz') {
     quiz.style.zIndex = 1;
+    option1.style.zIndex = 2;
+    option2.style.zIndex = 2;
+    option3.style.zIndex = 2;
+    option4.style.zIndex = 2;
+    option5.style.zIndex = 2;
+    option6.style.zIndex = 2;
     info.style.zIndex = 0;
+    toNext.style.zIndex = 0;
   } else {
     info.style.zIndex = 1;
+    toNext.style.zIndex = 2;
     quiz.style.zIndex = 0;
+    option1.style.zIndex = 0;
+    option2.style.zIndex = 0;
+    option3.style.zIndex = 0;
+    option4.style.zIndex = 0;
+    option5.style.zIndex = 0;
+    option6.style.zIndex = 0;
   }
 }
 
+
+function numToPxString(num) {
+  return (num.toString() + 'px');
+}
+
+function arrange() {
+  base = document.getElementById('base');
+  quiz = document.getElementById('canvas-quiz');
+  info = document.getElementById('canvas-info');
+
+  width = Math.min(base.clientWidth, 600);
+  quiz.width = width;
+  info.width = width;
+
+  height = width * 1.0;
+  base.style.height = numToPxString(height * 1.1);
+  quiz.height = height;
+  info.height = height;
+
+  cxq = quiz.getContext('2d');
+  cxq.fillStyle = 'aliceblue';
+  cxq.fillRect(0, 0, quiz.width, quiz.height);
+  cxi = info.getContext('2d');
+  cxi.fillStyle = 'aliceblue';
+  cxi.fillRect(0, 0, info.width, info.height);
+
+  option1 = document.getElementById('option1');
+  option2 = document.getElementById('option2');
+  option3 = document.getElementById('option3');
+  option4 = document.getElementById('option4');
+  option5 = document.getElementById('option5');
+  option6 = document.getElementById('option6');
+  toNext  = document.getElementById('to-next');
+
+  const MARGIN_RATE = 0.02;
+  margin = width * MARGIN_RATE;
+  buttonWidth = (width - margin * 4) / 3;
+  buttonHeight = (height * 0.4 - margin * 3) / 2;
+  
+  option1.style.top = numToPxString(height * 0.6 + margin);
+  option2.style.top = numToPxString(height * 0.6 + margin);
+  option3.style.top = numToPxString(height * 0.6 + margin);
+  option1.style.left = numToPxString(margin);
+  option2.style.left = numToPxString(margin * 2 + buttonWidth);
+  option3.style.left = numToPxString(margin * 3 + buttonWidth * 2);
+  option1.style.width = numToPxString(buttonWidth);
+  option2.style.width = numToPxString(buttonWidth);
+  option3.style.width = numToPxString(buttonWidth);
+  option1.style.height = numToPxString(buttonHeight);
+  option2.style.height = numToPxString(buttonHeight);
+  option3.style.height = numToPxString(buttonHeight);
+  option4.style.top = numToPxString(height * 0.6 + buttonHeight + margin * 2);
+  option5.style.top = numToPxString(height * 0.6 + buttonHeight + margin * 2);
+  option6.style.top = numToPxString(height * 0.6 + buttonHeight + margin * 2);
+  option4.style.left = numToPxString(margin);
+  option5.style.left = numToPxString(margin * 2 + buttonWidth);
+  option6.style.left = numToPxString(margin * 3 + buttonWidth * 2);
+  option4.style.width = numToPxString(buttonWidth);
+  option5.style.width = numToPxString(buttonWidth);
+  option6.style.width = numToPxString(buttonWidth);
+  option4.style.height = numToPxString(buttonHeight);
+  option5.style.height = numToPxString(buttonHeight);
+  option6.style.height = numToPxString(buttonHeight);
+  toNext.style.top = numToPxString(height * 0.7 + margin);
+  toNext.style.left = numToPxString(margin * 2 + buttonWidth);
+  toNext.style.width = numToPxString(buttonWidth);
+  toNext.style.height = numToPxString(buttonHeight);
+}
+
+function createQuiz(beforeUnit, afterUnit) {
+  option1.innerHTML = '0.001 ' + afterUnit;
+  option2.innerHTML = '0.01 ' + afterUnit;
+  option3.innerHTML = '0.1 ' + afterUnit;
+  option4.innerHTML = '10 ' + afterUnit;
+  option5.innerHTML = '100 ' + afterUnit;
+  option6.innerHTML = '1000 ' + afterUnit;
+}
+
+function drawText(canvas, text) {
+  ctx = canvas.getContext('2d');
+  ctx.fillStyle='#303030';
+  ctx.font='60px "Yu Gothic"';
+  ctx.textAlign='center';
+  ctx.textBaseline='top';
+  ctx.fillText(text, canvas.width/2, 0);
+}
 
 function drawChara(context, imagePath, x, y, width) {
   chara = new Image();
@@ -41,20 +143,13 @@ function createResult() {
 }
 
 /* initialize at page load. */
-base = document.getElementById('base');
-quiz = document.getElementById('canvas-quiz');
-info = document.getElementById('canvas-info');
-
-w = base.clientWidth;
-quiz.width = w;
-info.width = w;
-quiz.height = w * 0.5;
-info.height = w * 0.5;
-base.style.height = ((w * 0.55).toString(10) + "px");
+arrange();
 
 window.onload = function() { 
   $('#button-quiz').on('click', function() {
     // alert("quizクリックされました");
+    createQuiz('L','mL');
+    drawText(quiz, '1dLは何Lかな？');
     changeLayer('quiz');
   });
   
